e912ef41ff887ce36acf562e78fc4bcccc063879
Georgios Karametas <geometas@hotmail.com>
2017-03-31 07:56:13 +0300

    Add /todos and /todos/:id

--- server.js
+++ server.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'Meet mom for lunch',
+	completed: false
+}, {
+	id: 2,
+	description: 'Go to market',
+	completed: false
+}, {
+	id: 3,
+	description: 'Feed the cat',
+	completed: true
+}];
 
 app.get('/', function(req, res) {
 	res.send('Todo API Root');
+});
+
+app.get('/todos', function(req, res) {
+	res.json(todos);
+});
+
+app.get('/todos/:id', function(req, res) {
+	var todo = todos.filter(function(todo) {
+		return todo.id.toString() === req.params.id;
+	});
+
+	if (todo.length === 0) {
+		res.status(404).send();
+	} else {
+		res.json(todo);
+	}
 });
 
 app.listen(PORT, function() {
