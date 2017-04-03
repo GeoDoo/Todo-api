4f4e067be3b2ce6b389a78db19d986bc221661ab
Georgios Karametas <geometas@hotmail.com>
2017-04-03 07:40:34 +0300

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -16,22 +18,27 @@ app.get('/todos', function(req, res) {
 });
 
 app.get('/todos/:id', function(req, res) {
-	var todo = todos.filter(function(todo) {
-		return todo.id.toString() === req.params.id;
-	});
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
-	if (todo.length === 0) {
-		res.status(404).send();
+	if (matchedTodo) {
+		res.json(matchedTodo);
 	} else {
-		res.json(todo);
+		res.status(404).send();
 	}
 });
 
 app.post('/todos', function(req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
 	body.id = todoNextId++;
 	todos.push(body);
-	res.json(todos);
+	res.json(body);
 });
 
 app.listen(PORT, function() {
