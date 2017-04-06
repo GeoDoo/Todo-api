fc6ff13f494c1375409bcaf7f85234ab001ebc4d
Georgios Karametas <geometas@hotmail.com>
2017-04-06 20:31:37 +0300

    Add variable playground

---
+++ variable-playground.js
@@ -0,0 +1,25 @@
+// var person = {
+// 	name: 'Andrew',
+// 	age: 21
+// }
+
+// function updatePerson(obj) {
+// 	// obj = {
+// 	// 	name: 'Andrew',
+// 	// 	age: 24
+// 	// }
+// 	// obj.age = 24;
+// }
+
+var grades = [15,88];
+
+function addGrades(gradesArr) {
+	gradesArr.push(55);
+	debugger;
+
+	// gradesArr = [12, 92];
+	// grades = [12,99];
+}
+
+addGrades(grades);
+console.log(grades);
