671854fb87a49da15d395d47ed1a9a32b8e1f930
Georgios Karametas <geometas@hotmail.com>
2017-04-25 08:06:08 +0300

    Add todo/user association

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -19,42 +19,41 @@ var Todo = sequelize.define('todo', {
 	}
 });
 
-sequelize.sync().then(function() {
+var User = sequelize.define('user', {
+	email: {
+		type: Sequelize.STRING
+	}
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
+sequelize.sync({
+		// force: true
+	}).then(function() {
 	console.log('Everything is synced');
 
-	Todo.findById(2).then(function(todo) {
-		if (todo) {
-			console.log(todo.toJSON())
-		} else {
-			console.log('No match found!');
-		}
+	User.findById(1).then(function(user) {
+		user.getTodos({
+			where: {
+				completed: true
+			}
+		}).then(function(todos) {
+			todos.forEach(function(todo) {
+				console.log(todo.toJSON());
+			});
+		});
 	});
 
-	// Todo.create({
-	// 	desctiption: "Take out trash"
-	// }).then(function(todo) {
-	// 	return Todo.create({
-	// 		desctiption: 'Clean office'
-	// 	});
+	// User.create({
+	// 	email: 'geodoo@geodoo.work'
 	// }).then(function() {
-	// 	// return Todo.findById(1);
-	// 	return Todo.findAll({
-	// 		where: {
-	// 			desctiption: {
-	// 				$like: '%Office%'
-	// 			}
-	// 		}
+	// 	return Todo.create({
+	// 		desctiption: 'Clean yard'
 	// 	});
-	// }).then(function(todos) {
-	// 	if (todos) {
-	// 		todos.forEach(function(todo) {
-	// 			console.log(todo.toJSON());
-	// 		});
-	// 	} else {
-	// 		console.log('No todo found!');
-	// 	}
-	// }).catch(function(e) {
-	// 	console.log(e);
+	// }).then(function(todo) {
+	// 	User.findById(1).then(function(user) {
+	// 		user.addTodo(todo);
+	// 	});
 	// });
-
 });
